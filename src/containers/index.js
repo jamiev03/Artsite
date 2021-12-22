d515394c4aec8efbea3bdef840660df70879c5aa
unknown <jamie03.v@gmail.com>
2021-12-21 20:12:29 -0500

    redo components

--- src/containers/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Features } from "./features/Features";
-export { default as Footer } from "./footer/Footer";
-export { default as Header } from "./header/Header";
-export { default as Whatis } from "./whatis/Whatis";
